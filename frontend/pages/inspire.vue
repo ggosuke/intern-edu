a44e8ee63a6dd447891d55f7346770e5324599a1
Ei Shiba <eysm207@gmail.com>
2022-12-09 10:36:07 +1100

    Revert "second commit"
    This reverts commit c28b0050cec14699af7e428b9a574a357bfa6e04.

--- frontend/pages/inspire.vue
+++ frontend/pages/inspire.vue
@@ -12,9 +12,9 @@
                 <v-card
                   color="#385F73"
                   dark
-                  >
+                >
                   <v-card-title class="text-h5">
-                    課題名
+                    授業名
                   </v-card-title>
 
                   <div class="text-center">
@@ -23,7 +23,7 @@
                       color="red"
                       text-color="white"
                     >
-                      授業名
+                      Red Chip
                     </v-chip>
 
                     <v-chip
@@ -31,118 +31,155 @@
                       color="green"
                       text-color="white"
                     >
-                      期日
+                      Green Chip
                     </v-chip>
                   </div>
                   
                   <v-card-subtitle
-                    class ="text-center" >
+                    class ="text-left" >
 
                   
-                    課題内容xxxxxxxxxxxxxxxxx
+                    課題内容。。。
                   </v-card-subtitle>
 
-                  <v-card-actions >
-                    <v-col class ="text-right">
-                      <v-btn>
-                        完了
-                      </v-btn>
-                    </v-col>
+                  <v-card-actions>
+                    <v-btn text
+                      class = "text-right">
+                      完了
+                    </v-btn>
                   </v-card-actions>
                 </v-card>
-
-                <v-col>
-                </v-col>
-
-                <v-card
-                  color="#385F73"
-                  dark
-                  >
-                  <v-card-title class="text-h5">
-                    課題名
-                  </v-card-title>
-
-                  <div class="text-center">
-                    <v-chip
-                      class="ma-2"
-                      color="red"
-                      text-color="white"
-                    >
-                      授業名
-                    </v-chip>
-
-                    <v-chip
-                      class="ma-2"
-                      color="green"
-                      text-color="white"
-                    >
-                      期日
-                    </v-chip>
-                  </div>
-                  
-                  <v-card-subtitle
-                    class ="text-center" >
-
-                  
-                    課題内容xxxxxxxxxxxxxxxxx
-                  </v-card-subtitle>
-
-                  <v-card-actions >
-                    <v-col class ="text-right">
-                      <v-btn>
-                        完了
-                      </v-btn>
-                    </v-col>
-                  </v-card-actions>
-                </v-card>
-
-                <v-col>
-                </v-col>
-
-                <v-card
-                  color="#385F73"
-                  dark
-                  >
-                  <v-card-title class="text-h5">
-                    課題名
-                  </v-card-title>
-
-                  <div class="text-center">
-                    <v-chip
-                      class="ma-2"
-                      color="red"
-                      text-color="white"
-                    >
-                      授業名
-                    </v-chip>
-
-                    <v-chip
-                      class="ma-2"
-                      color="green"
-                      text-color="white"
-                    >
-                      期日
-                    </v-chip>
-                  </div>
-                  
-                  <v-card-subtitle
-                    class ="text-center" >
-
-                  
-                    課題内容xxxxxxxxxxxxxxxxx
-                  </v-card-subtitle>
-
-                  <v-card-actions >
-                    <v-col class ="text-right">
-                      <v-btn>
-                        完了
-                      </v-btn>
-                    </v-col>
-                  </v-card-actions>
-                </v-card>
-
               </v-col>
 
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
+              >
+                <v-card
+                  :color="item.color"
+                  dark
+                >
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="text-h5"
+                        v-text="item.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          outlined
+                          rounded
+                          small
+                        >
+                          START RADIO
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar
+                      class="ma-3"
+                      size="125"
+                      tile
+                    >
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+                    <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-card
+                  color="#385F73"
+                  dark
+                >
+                  <v-card-title class="text-h5">
+                    Unlimited music now
+                  </v-card-title>
+
+                  <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+                  <v-card-actions>
+                    <v-btn text>
+                      完了
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
+              >
+                <v-card
+                  :color="item.color"
+                  dark
+                >
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="text-h5"
+                        v-text="item.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          outlined
+                          rounded
+                          small
+                        >
+                          START RADIO
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar
+                      class="ma-3"
+                      size="125"
+                      tile
+                    >
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
             </v-row>
           </v-container>
 
