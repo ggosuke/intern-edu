59ed198a18a3d8640d084b225d6c7220f08aa99d
Ei Shiba <eysm207@gmail.com>
2022-12-08 17:59:18 +1100

    shibas first commit

--- frontend/pages/inspire.vue
+++ frontend/pages/inspire.vue
@@ -1,18 +1,192 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+      <template>
+        <v-card
+          max-width="400"
+          class="mx-auto"
+        >
+          <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-card
+                  color="#385F73"
+                  dark
+                >
+                  <v-card-title class="text-h5">
+                    授業名
+                  </v-card-title>
+
+                  <div class="text-center">
+                    <v-chip
+                      class="ma-2"
+                      color="red"
+                      text-color="white"
+                    >
+                      Red Chip
+                    </v-chip>
+
+                    <v-chip
+                      class="ma-2"
+                      color="green"
+                      text-color="white"
+                    >
+                      Green Chip
+                    </v-chip>
+                  </div>
+                  
+                  <v-card-subtitle
+                    class ="text-left" >
+
+                  
+                    課題内容。。。
+                  </v-card-subtitle>
+
+                  <v-card-actions>
+                    <v-btn text
+                      class = "text-right">
+                      完了
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
+              >
+                <v-card
+                  :color="item.color"
+                  dark
+                >
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="text-h5"
+                        v-text="item.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          outlined
+                          rounded
+                          small
+                        >
+                          START RADIO
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar
+                      class="ma-3"
+                      size="125"
+                      tile
+                    >
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+                    <v-container>
+            <v-row dense>
+              <v-col cols="12">
+                <v-card
+                  color="#385F73"
+                  dark
+                >
+                  <v-card-title class="text-h5">
+                    Unlimited music now
+                  </v-card-title>
+
+                  <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+
+                  <v-card-actions>
+                    <v-btn text>
+                      完了
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+
+              <v-col
+                v-for="(item, i) in items"
+                :key="i"
+                cols="12"
+              >
+                <v-card
+                  :color="item.color"
+                  dark
+                >
+                  <div class="d-flex flex-no-wrap justify-space-between">
+                    <div>
+                      <v-card-title
+                        class="text-h5"
+                        v-text="item.title"
+                      ></v-card-title>
+
+                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                      <v-card-actions>
+                        <v-btn
+                          v-if="item.artist === 'Ellie Goulding'"
+                          class="ml-2 mt-3"
+                          fab
+                          icon
+                          height="40px"
+                          right
+                          width="40px"
+                        >
+                          <v-icon>mdi-play</v-icon>
+                        </v-btn>
+
+                        <v-btn
+                          v-else
+                          class="ml-2 mt-5"
+                          outlined
+                          rounded
+                          small
+                        >
+                          START RADIO
+                        </v-btn>
+                      </v-card-actions>
+                    </div>
+
+                    <v-avatar
+                      class="ma-3"
+                      size="125"
+                      tile
+                    >
+                      <v-img :src="item.src"></v-img>
+                    </v-avatar>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+
+        </v-card>
+      </template>
       <blockquote class="blockquote">
-        {{ data.message }}
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
+        
       </blockquote>
     </v-col>
   </v-row>
@@ -30,9 +204,6 @@ export default {
     }
   },
   // 通信で取得するデータ
-  async asyncData () {
-    const data = await axios.get('/api/hello')
-    return data.data
-  }
+
 }
 </script>
