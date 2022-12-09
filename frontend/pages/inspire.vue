314af7b59021c909971d7b696f37e021d9e8996e
Ei Shiba <eysm207@gmail.com>
2022-12-09 15:23:02 +1100

    card date change

--- frontend/pages/inspire.vue
+++ frontend/pages/inspire.vue
@@ -10,8 +10,8 @@
             <v-row dense>
               <v-col cols="12">
                 <v-card
-                  color="#385F73"
-                  dark
+                  color="white"
+                  light
                   class="ma-2"
                   v-for = "(item,index) in items"
                   >
@@ -19,29 +19,33 @@
                     {{item.task}}
                   </v-card-title>
 
-                  <div class="text-center">
-                    <v-chip
-                      class="ma-2"
-                      color="red"
+                  <div class="text-left ml-3" >
+                    <v-chip label
+                      class="ma-1"
+                      color="blue"
                       text-color="white"
+                      
+
                     >
                       {{item.lesson}}
                     </v-chip>
 
-                    <v-chip
-                      class="ma-2"
+                    <v-chip label
+                      class="ma-1"
+                      style= "font-weight:bold" 
                       color="green"
                       text-color="white"
                     >
-                      {{item.due}}
+                      あと
+                      {{remainDay(item.due)}}
+                      日！
                     </v-chip>
                   </div>
                   
                   <v-card-subtitle
-                    class ="text-center" >
-
+                    class ="text-left" >
                   
-                    課題内容xxxxxxxxxxxxxxxxx
+                    {{item.task_detail}}
                   </v-card-subtitle>
 
                   <v-card-actions >
@@ -49,6 +53,7 @@
                       <v-btn @click="done(index)">
                         完了
                       </v-btn>
+                    
                     </v-col>
                   </v-card-actions>
                 </v-card>
@@ -76,16 +81,34 @@ export default {
     return {
       items: [{
         lesson_id: 0,
-        task: "task1",
-        due: "12/1",
-        lesson: "lesson1",
-        task_detail: "task1xxxx"
+        task: "課題1",
+        due: "2022/12/20",
+        lesson: "線形代数1",
+        task_detail: "練習問題2-20"
       },{
         lesson_id: 1,
-        task: "task2",
-        due: "12/1",
-        lesson: "lesson2",
-        task_detail: "task2xxxx"
+        task: "課題2",
+        due: "2023/1/20",
+        lesson: "古典中世音楽",
+        task_detail: "歴史について"
+      },{
+        lesson_id: 2,
+        task: "課題3",
+        due: "2023/2/23",
+        lesson: "古典中世音楽",
+        task_detail: "歴史について"
+      },{
+        lesson_id: 3,
+        task: "課題4",
+        due: "2023/1/22",
+        lesson: "古典中世音楽",
+        task_detail: "歴史について"
+      },{
+        lesson_id: 4,
+        task: "課題5",
+        due: "2022/12/15",
+        lesson: "古典中世音楽",
+        task_detail: "歴史について"
       }
       ]
     }
@@ -95,7 +118,23 @@ export default {
     done(index) {
       this.items.splice(index, 1)
       console.log(index)
+    },
+
+    remainDay(due) {
+      let date = new Date();
+      let dueDate = new Date(due);
+      let remain = parseInt((dueDate - date) / 1000/ 60/ 60/ 24);
+
+      return remain
     }
+
+    
+
+
+
+    
+
+
   }
 }
 </script>
